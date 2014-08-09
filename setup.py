e08f7a86e65b6451782cde0321b53ea3b1d58e9e
Samuel Sutch <sam@sutch.net>
2014-08-09 10:34:23 -0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lexical-uuid",
-    version="0.1.0",
+    version="0.1.1",
     url='http://github.com/samuraisam/lexical_uuid',
     author='Samuel Sutch',
     author_email='sam@sutch.net',
